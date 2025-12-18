f2661479601a4fa8145495eb6c191b5390433a7e
karthi <karhtik2@gmail.com>
2025-12-18 16:14:21 +0530

    Add HTML frontend for prediction

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import pickle
 import os
 
@@ -9,15 +9,33 @@ with open("data/model.pkl", "rb") as f:
 
 @app.route("/")
 def home():
-    return "Boston Housing Price Prediction API is LIVE 🚀"
+    return render_template("index.html")
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    data = request.json["features"]
-    prediction = model.predict([data])
-    return jsonify({"predicted_price": prediction[0]})
+    features = [
+        float(request.form["CRIM"]),
+        float(request.form["ZN"]),
+        float(request.form["INDUS"]),
+        float(request.form["CHAS"]),
+        float(request.form["NOX"]),
+        float(request.form["RM"]),
+        float(request.form["AGE"]),
+        float(request.form["DIS"]),
+        float(request.form["RAD"]),
+        float(request.form["TAX"]),
+        float(request.form["PTRATIO"]),
+        float(request.form["B"]),
+        float(request.form["LSTAT"]),
+    ]
+
+    prediction = model.predict([features])[0]
+
+    return render_template(
+        "index.html",
+        prediction_text=f"Predicted House Price: {prediction:.2f}"
+    )
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
